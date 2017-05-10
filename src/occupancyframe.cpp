1ffddb067e8e56a637999fa8039b1b5b73ac60a9
Henry Warhurst <hwar3828@uni.sydney.edu.au>
2017-05-10 21:06:47 +1000

    Fix depth index computation bug

--- src/occupancyframe.cpp
+++ src/occupancyframe.cpp
@@ -2,6 +2,9 @@
 
 #include <iostream>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
 OccupancyFrame::OccupancyFrame(const unsigned char *depth, size_t width, size_t height)
 	: depth_(depth),
 	  width_(width),
@@ -14,18 +17,32 @@ bool OccupancyFrame::compute()
 	const size_t startZ 	= 1;
 	const size_t stepZ 		= 100;
 
+	// TODO: Delete PCL stuff once we verify the clouds are correct
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	size_t nPoints = width_*height_;
+	std::cout << std::endl;
+	std::cout << "nPoints = " << nPoints << std::endl;
+	cloud.width = nPoints;
+	cloud.height = 1;
+	cloud.is_dense = true;
+	cloud.points.resize (cloud.width * cloud.height);
+	std::cout << "Number of points in the cloud is " << cloud.points.size() << std::endl;
+
 	for (size_t v=0; v<height_; ++v) {
 		for (size_t u=0; u<width_; ++u) {
-			size_t depthIdx = v*height_ + u*width_;
+			size_t depthIdx = v*height_ + u;
 			double curDepth = (double) depth_[depthIdx];
-
 			// TODO: Delete this, it's just to check the pointcloud
 			// is generated properly ******************************
 			double tstZ = curDepth / kCorrectionFactor;
 			double tstX = (u - kOpticalCentreX)*tstZ / kFocalLengthX;
 			double tstY = (v - kOpticalCentreY)*tstZ / kFocalLengthY;
 			// Send to pcl and save as pcd file to view
-			
+			cloud.points[depthIdx].x = tstX;
+			cloud.points[depthIdx].y = tstY;
+			cloud.points[depthIdx].z = tstZ;
+
+		
 			// ****************************************************
 
 			for (size_t rayZ=startZ; rayZ<maxZ; rayZ+=stepZ) {
@@ -43,12 +60,15 @@ bool OccupancyFrame::compute()
 				z_.push_back(curZ);
 				occupancy_.push_back(curOccupancy);
 			
-				std::cout << "rayZ = " << rayZ << std::endl;	
-				std::cout << "curDepth = " << curDepth << std::endl;
-				std::cout << "Current occupancy = " << curOccupancy << std::endl;
+//				std::cout << "rayZ = " << rayZ << std::endl;	
+//				std::cout << "curDepth = " << curDepth << std::endl;
+//				std::cout << "Current occupancy = " << curOccupancy << std::endl;
 			}	
 		}
 	}
+	
+	// TODO: Delete PCL stuff
+	//pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
 	
 	return true;
 }
